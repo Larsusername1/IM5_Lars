5d05162a93b7ae1b73642a94bd738c72d850428a
Larsusername1 <153275901+Larsusername1@users.noreply.github.com>
2023-12-08 20:25:37 +0100

    commit

--- js/skript.js
+++ js/skript.js
@@ -29,5 +29,25 @@ window.onload = function() {
   showSlides();
 };
 
-console.log("Hallo");
+console.log('Hello from script.js');
+// Hier wird überprüft, ob das Element mit der ID 'bestellen' vorhanden ist, bevor ein Event Listener hinzugefügt wird
+var bestellenButton = document.getElementById('bestellen');
+if (bestellenButton) {
+  bestellenButton.addEventListener('click', function () {
+    // Get the number of CDs entered by the user
+    var numberOfCDs = document.getElementById('anzahl_cds').value;
+
+    // Ensure the input is a valid number
+    if (isNaN(numberOfCDs) || numberOfCDs < 1 || numberOfCDs > 5) {
+      alert('Please enter a valid number of CDs between 1 and 5.');
+      return;
+    }
+
+    // Calculate the total price (price per CD is 10 CHF)
+    var totalPrice = numberOfCDs * 27.35;
+
+    // Display the total price next to the button
+    document.getElementById('priceDisplay').textContent = 'Total Price: ' + totalPrice + ' CHF';
+  });
+}
 
--- kontakt.php
+++ kontakt.php
@@ -1,36 +1,66 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Empfänger-E-Mail-Adresse
-    $to_email = 'lars.hanselmann@hotmail.com'; // Hier deine tatsächliche E-Mail-Adresse eintragen
+$to_email = 'lars.hanselmann@hotmail.com';
+$subject = 'Kontaktformular - Lars Hanselmann Filmwerk';
+$body = $_POST['message'];
+$headers = 'From: ' . $_POST['email'];
 
-    // Betreff der E-Mail
-    $subject = 'Neue Kontaktanfrage';
+if (mail($to_email, $subject, $body, $headers)) {
+    echo '<!DOCTYPE html>
+    <html lang="de">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <meta name="description" content="Erstellung, Gestaltung von multimedialen Werken. Der Fokus liegt auf audiovisuellen Beiträgen, sprich Filmformate wie Dokumentation, Reportage, Nachricht, Eventfilm sowie Werbefilm. Diese können real (nonfiktional) sowie erfunden (fiktional) sein.">
+    
+        <title>Lars Hanselmann Filmwerk | Kontakt</title>
+    
+        <link rel="stylesheet" href="css/style.css">
+    
+        <script src="js/skript.js"></script>
+    
+    </head>
+    
+    <body>
+        <!-- Studentische Arbeit im Rahmen der Bachelorthesis MMP -->
+        <header class="site-header">
+            <div class="Banner">
+                <img src="img/Banner.jpg" alt="Banner">
+            </div>
+        </header>
 
-    // Formulardaten abrufen
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    // E-Mail-Inhalt
-    $body = "Name: $name\n";
-    $body .= "E-Mail: $email\n";
-    $body .= "Nachricht:\n$message";
-
-    // Zusätzliche Header
-    $headers = 'From: ' . $email . "\r\n" .
-        'Reply-To: ' . $email . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
-
-    // Versuche, die E-Mail zu senden
-    if (mail($to_email, $subject, $body, $headers)) {
-        echo "Vielen Dank! Ihre Nachricht wurde erfolgreich gesendet.";
-        echo '<script>
-                setTimeout(function(){
-                    window.location.href = "https://lhfilmwerk.ch/kontakt.html";
-                }, 5000); // 5000 Millisekunden entsprechen 5 Sekunden
-              </script>';
-    } else {
-        echo "Fehler beim Versenden der E-Mail. Bitte versuchen Sie es später erneut.";
-    }
+        <!-- Menue -->
+        <nav class="site-nav">
+            <ul class="site-nav-list">
+                <li><a href="index.html">Startseite</a></li>
+                <li><a href="news.html">News</a></li>
+                <li><a href="uebermich.html">Über mich</a></li>
+                <li><a href="kontakt.html">Kontakt</a></li>
+            </ul>
+        </nav>
+        <!-- Ende Menue -->
+    
+        <!-- Erfolgsmeldung -->
+        <div class="success-message">
+            <p>Vielen Dank! Ihre Nachricht wurde erfolgreich gesendet.</p>
+        </div>
+    
+        <!-- Footer -->
+        <footer class="site-footer">
+            <a href="#top">Nach oben</a>
+        </footer>
+    
+        <!-- JavaScript-Weiterleitung nach 5 Sekunden -->
+        <script>
+            setTimeout(function () {
+                window.location.href = "https://lhfilmwerk.ch/kontakt.html";
+            }, 5000); // 5000 Millisekunden entsprechen 5 Sekunden
+        </script>
+    
+    </body>
+    
+    </html>';
+} else {
+    echo "Fehler beim Versenden der E-Mail. Bitte versuchen Sie es später erneut.";
 }
 ?>
